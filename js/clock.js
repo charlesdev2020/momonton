c39c1a1efb0eda860a8fb78233c2597f81936377
charlesdev2020 <75482355+charlesdev2020@users.noreply.github.com>
2021-03-05 17:31:13 +0900

    #4.3 Making a JS Clock part

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+const clockContainer = document.querySelector(".js-clock .clock__text");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const time = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clockContainer.innerHTML = time;
+  return;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
